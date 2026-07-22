167f17244725b763570e0c87b29941c70ab72a05
Franklyn <franklyniyala@gmail.com>
2026-07-22 21:18:34 +0100

    fix: improve backend quality checks and test coverage

--- backend-drf/users/serializers.py
+++ backend-drf/users/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
+
 User = get_user_model()
 
 
@@ -13,7 +14,6 @@ class UserRegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
-        
         return user
 
 
